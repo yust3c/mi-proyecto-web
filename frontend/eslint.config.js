cd4b69eaaa065343083bd7b0e675853d71ac9d42
Nicolás <147382015+yust3c@users.noreply.github.com>
2026-05-23 12:17:15 +0200

    Update eslint.config.js

--- frontend/eslint.config.js
+++ frontend/eslint.config.js
@@ -1,11 +1,22 @@
 import js from '@eslint/js';
 import pluginReact from 'eslint-plugin-react';
+import babelParser from '@babel/eslint-parser';
 
 export default [
   js.configs.recommended,
   {
+    files: ['**/*.js', '**/*.jsx'],
     plugins: {
       react: pluginReact,
+    },
+    languageOptions: {
+      parser: babelParser,
+      parserOptions: {
+        requireConfigFile: false,
+        babelOptions: {
+          presets: ['@babel/preset-react'],
+        },
+      },
     },
     settings: {
       react: {
